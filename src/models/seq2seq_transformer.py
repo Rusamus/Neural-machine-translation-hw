720f94b099f28e681537fcf1516d9087523d4051
Ruslan Musaev <r.musaev@gpu14.vlabs>
2023-05-12 01:47:17 +0300

    PEP8

--- src/models/seq2seq_transformer.py
+++ src/models/seq2seq_transformer.py
@@ -2,14 +2,14 @@ import torch
 import numpy as np
 import math
 import metrics
+
 from models.positional_encoding import PositionalEncoding
 
 
 class Seq2SeqTransformer(torch.nn.Module):
     def __init__(self, model_config, word2index):
         super().__init__()
-       # TODO: Реализуйте конструктор seq2seq трансформера - матрица эмбеддингов, позиционные эмбеддинги, encoder/decoder трансформер, vocab projection head
-        
+
         src_vocab_size = model_config['src_vocab_size']
         tgt_vocab_size = model_config['tgt_vocab_size']
         nhead = model_config['nhead']
@@ -23,12 +23,14 @@ class Seq2SeqTransformer(torch.nn.Module):
 
         self.src_embed = torch.nn.Embedding(src_vocab_size, self.embedding_size)
         self.tgt_embed = torch.nn.Embedding(tgt_vocab_size, self.embedding_size)
-        self.positional_encoding = PositionalEncoding(self.embedding_size, self.maxlen, device=self.device)
+        self.positional_encoding = PositionalEncoding(
+            self.embedding_size, self.maxlen, device=self.device
+        )
 
         self.transformer_model = torch.nn.Transformer(
-                d_model=self.embedding_size, nhead=nhead, 
-                num_encoder_layers=num_encoder_layers
-            )
+            d_model=self.embedding_size, nhead=nhead,
+            num_encoder_layers=num_encoder_layers
+        )
         self.fc = torch.nn.Linear(self.embedding_size, tgt_vocab_size)
         self.criterion = torch.nn.CrossEntropyLoss()
 
@@ -37,70 +39,90 @@ class Seq2SeqTransformer(torch.nn.Module):
         parameters.append({'params': self.tgt_embed.parameters()})
         parameters.append({'params': self.transformer_model.parameters()})
         parameters.append({'params': self.fc.parameters()})
-        
-        self.optimizer = torch.optim.AdamW(parameters, lr=lr, weight_decay=weight_decay)
-        self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max)
+
+        self.optimizer = torch.optim.AdamW(
+            parameters, lr=lr, weight_decay=weight_decay
+        )
+        self.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            self.optimizer, T_max
+        )
         self.word2index = word2index
 
-
     def generate_tgt_msk(self, tgt):
-        mask = torch.triu(torch.ones(tgt.shape[1], tgt.shape[1], device=self.device) * float('-inf'), diagonal=1)
+        mask = torch.triu(
+            torch.ones(tgt.shape[1], tgt.shape[1],
+                       device=self.device) * float('-inf'), diagonal=1
+        )
         return mask
 
     def generate_src_msk(self, src):
-        mask = torch.zeros((src.shape[1], src.shape[1]), 
-            device=self.device).type(torch.bool)
+        mask = torch.zeros(
+            (src.shape[1], src.shape[1]), device=self.device).type(torch.bool)
         return mask
-    
+
     def generate_all_masks(self, src, tgt=None):
         src_mask = self.generate_src_msk(src)
         if tgt is not None:
             tgt_mask = self.generate_tgt_msk(tgt)
-            tgt_key_padding_mask = (tgt == self.word2index["[PAD]"]).type(torch.bool)
+            tgt_key_padding_mask = (
+                tgt == self.word2index["[PAD]"]).type(torch.bool)
         else:
             tgt_mask = None
             tgt_key_padding_mask = None
 
-        src_key_padding_mask = (src == self.word2index["[PAD]"]).type(torch.bool)
+        src_key_padding_mask = (
+            src == self.word2index["[PAD]"]).type(torch.bool)
         return src_mask, tgt_mask, src_key_padding_mask, tgt_key_padding_mask
 
-    def forward(self, src, tgt, src_mask=None, tgt_mask=None, src_key_padding_mask=None, tgt_key_padding_mask=None): 
+    def forward(self, src, tgt, src_mask=None, tgt_mask=None,
+                src_key_padding_mask=None, tgt_key_padding_mask=None):
 
-        src_embed = self.src_embed(src) * math.sqrt(self.embedding_size) 
-        src_embed = self.positional_encoding(src_embed) 
-        tgt_embed = self.tgt_embed(tgt) * math.sqrt(self.embedding_size) 
+        src_embed = self.src_embed(src) * math.sqrt(self.embedding_size)
+        src_embed = self.positional_encoding(src_embed)
+        tgt_embed = self.tgt_embed(tgt) * math.sqrt(self.embedding_size)
         tgt_embed = self.positional_encoding(tgt_embed)
 
-        src_embed = src_embed.permute([1,0,2])
-        tgt_embed = tgt_embed.permute([1,0,2])
-        
-        out = self.transformer_model(src_embed, 
-            tgt_embed, src_mask=src_mask, 
-            tgt_mask=tgt_mask, memory_mask=None, 
-            src_key_padding_mask=src_key_padding_mask, 
+        src_embed = src_embed.permute([1, 0, 2])
+        tgt_embed = tgt_embed.permute([1, 0, 2])
+
+        out = self.transformer_model(
+            src_embed,
+            tgt_embed,
+            src_mask=src_mask,
+            tgt_mask=tgt_mask,
+            memory_mask=None,
+            src_key_padding_mask=src_key_padding_mask,
             tgt_key_padding_mask=tgt_key_padding_mask,
-            memory_key_padding_mask=None)
-        
+            memory_key_padding_mask=None
+        )
+
         output = self.fc(out)
-        output = output.permute([1,0,2])
+        output = output.permute([1, 0, 2])
 
         return output
 
-
     def training_step(self, batch):
-        
+
         input_tensor, target_tensor = batch
         self.optimizer.zero_grad()
 
-        src_mask, tgt_mask, src_key_padding_mask, tgt_key_padding_mask = self.generate_all_masks(input_tensor, target_tensor)
-        decoder_outputs = self.forward(input_tensor, target_tensor, src_mask=src_mask, tgt_mask=tgt_mask, src_key_padding_mask=src_key_padding_mask, tgt_key_padding_mask=tgt_key_padding_mask)
+        src_mask, tgt_mask, src_key_padding_mask, tgt_key_padding_mask = (
+            self.generate_all_masks(input_tensor, target_tensor)
+        )
+        decoder_outputs = self.forward(
+            input_tensor, target_tensor,
+            src_mask=src_mask, tgt_mask=tgt_mask,
+            src_key_padding_mask=src_key_padding_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask
+        )
         target_tensor = target_tensor[:, :, None]
         target_length = target_tensor.shape[1]
 
         loss = 0.0
         for di in range(target_length):
             loss += self.criterion(
-                decoder_outputs[:, di, :].squeeze(), target_tensor[:, di, :].squeeze()
+                decoder_outputs[:, di, :].squeeze(),
+                target_tensor[:, di, :].squeeze()
             )
         loss = loss / target_length
         loss.backward()
@@ -112,14 +134,21 @@ class Seq2SeqTransformer(torch.nn.Module):
     def validation_step(self, batch):
         input_tensor, target_tensor = batch
         with torch.no_grad():
-            src_mask, tgt_mask, src_key_padding_mask, tgt_key_padding_mask = self.generate_all_masks(input_tensor, target_tensor)
-            decoder_outputs = self.forward(input_tensor, target_tensor, src_mask=src_mask, tgt_mask=tgt_mask, src_key_padding_mask=src_key_padding_mask, tgt_key_padding_mask=tgt_key_padding_mask)
+            (src_mask, tgt_mask, src_key_padding_mask,
+            tgt_key_padding_mask) = self.generate_all_masks(input_tensor, target_tensor)
+            decoder_outputs = self.forward(
+                input_tensor, target_tensor,
+                src_mask=src_mask, tgt_mask=tgt_mask,
+                src_key_padding_mask=src_key_padding_mask,
+                tgt_key_padding_mask=tgt_key_padding_mask
+            )
             target_tensor = target_tensor[:, :, None]
             target_length = target_tensor.shape[1]
             loss = 0
             for di in range(target_length):
                 loss += self.criterion(
-                    decoder_outputs[:, di, :].squeeze(), target_tensor[:, di, :].squeeze()
+                    decoder_outputs[:, di, :].squeeze(),
+                    target_tensor[:, di, :].squeeze()
                 )
             loss = loss / target_length
 
@@ -130,48 +159,59 @@ class Seq2SeqTransformer(torch.nn.Module):
         predicted = predicted.squeeze().detach().cpu().numpy().astype(int)[:, 1:]
         actuals = target_tensor.squeeze().detach().cpu().numpy().astype(int)[:, 1:]
         bleu_score, actual_sentences, predicted_sentences = metrics.bleu_scorer(
-            predicted=predicted, actual=actuals, target_tokenizer=target_tokenizer
+            predicted=predicted, actual=actuals,
+            target_tokenizer=target_tokenizer
         )
         return bleu_score, actual_sentences, predicted_sentences
-    
+
+
     def encode(self, src, src_mask, src_key_padding_mask=None):
         src_embed = self.src_embed(src)
         src_embed = self.positional_encoding(src_embed)
-        src_embed = src_embed.permute([1,0,2])
-        encoded = self.transformer_model.encoder(src_embed, src_mask, src_key_padding_mask)
-        src_embed = src_embed.permute([1,0,2])
+        src_embed = src_embed.permute([1, 0, 2])
+        encoded = self.transformer_model.encoder(
+            src_embed, src_mask, src_key_padding_mask)
+        src_embed = src_embed.permute([1, 0, 2])
 
         return encoded
 
-    def decode(self, tgt, encoded, tgt_mask=None, src_mask=None, tgt_key_padding_mask=None,
-        src_key_padding_mask=None, memory_key_padding_mask=None):
+
+    def decode(self, tgt, encoded, tgt_mask=None, src_mask=None,
+            tgt_key_padding_mask=None, src_key_padding_mask=None,
+            memory_key_padding_mask=None):
 
         tgt_embed = self.tgt_embed(tgt)
         tgt_embed = self.positional_encoding(tgt_embed)
-        tgt_embed = tgt_embed.permute([1,0,2])
-        encoded = encoded.permute([1,0,2])
+        tgt_embed = tgt_embed.permute([1, 0, 2])
+        encoded = encoded.permute([1, 0, 2])
 
-        decoded = self.transformer_model.decoder(tgt_embed, encoded, 
-            tgt_mask=tgt_mask, memory_mask=src_mask, 
-            tgt_key_padding_mask=tgt_key_padding_mask, memory_key_padding_mask=memory_key_padding_mask
+        decoded = self.transformer_model.decoder(
+            tgt_embed, encoded,
+            tgt_mask=tgt_mask, memory_mask=src_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask,
+            memory_key_padding_mask=memory_key_padding_mask
         )
 
         return decoded
 
 
     def generate(self, src):
-        idx = torch.ones((src.shape[0], 1), device=self.device).fill_(self.word2index["[SEP]"]).type(torch.long)
-        src_mask, tgt_mask, src_key_padding_mask, tgt_key_padding_mask = self.generate_all_masks(src, idx)
+        idx = torch.ones((src.shape[0], 1), device=self.device).fill_(
+            self.word2index["[SEP]"]).type(torch.long)
+        (src_mask, tgt_mask, src_key_padding_mask,
+        tgt_key_padding_mask) = self.generate_all_masks(src, idx)
         encoded = self.encode(src, src_mask)
 
         for _ in range(self.maxlen):
-            output = self.decode(idx, encoded,
-                    tgt_mask=tgt_mask,
-                    src_mask=None,
-                    tgt_key_padding_mask=tgt_key_padding_mask,
-                    src_key_padding_mask=None,
-                    memory_key_padding_mask=src_key_padding_mask)
-            
+            output = self.decode(
+                idx, encoded,
+                tgt_mask=tgt_mask,
+                src_mask=None,
+                tgt_key_padding_mask=tgt_key_padding_mask,
+                src_key_padding_mask=None,
+                memory_key_padding_mask=src_key_padding_mask
+            )
+
             logits = self.fc(output)
             logits = logits[-1, :, :]
             probs = torch.nn.functional.softmax(logits, dim=-1).squeeze()
@@ -179,6 +219,7 @@ class Seq2SeqTransformer(torch.nn.Module):
             # generate new word based on probs
             idx_next = torch.multinomial(probs, num_samples=1)
             idx = torch.cat((idx, idx_next), dim=1)
-            src_mask, tgt_mask, src_key_padding_mask, tgt_key_padding_mask = self.generate_all_masks(src, idx)
- 
+            (src_mask, tgt_mask, src_key_padding_mask,
+            tgt_key_padding_mask) = self.generate_all_masks(src, idx)
+
         return idx
